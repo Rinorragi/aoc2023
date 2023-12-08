79eeb5b9b9c7106d7e161f198af9628664edfa92
Rinorragi <rinorragi@gmail.com>
2023-12-08 10:12:30 +0200

    Day 8: part 2

--- day08.fsx
+++ day08.fsx
@@ -30,26 +30,49 @@ let parseInput (filePath) =
         |> Map
     (lrSeq, instructions)
 
-let rec traverse (lrSeq: char array) (instructions: Map<string,Instruction>) (endKey: string) (key: string) (index: int64) =
-    if (key = endKey) then index
+let takeStep (lrSeq: char array) (lrSeqLength: int) (instructions: Map<string,Instruction>) (key: string) (index: int) = 
+    let newKey = 
+        match lrSeq.[index % lrSeqLength] with
+        | 'R' -> instructions[key].right
+        | 'L' -> instructions[key].left
+        | _ -> raise (System.ArgumentException("Unknown char"))
+    (newKey, (index + 1))
+
+let rec traverse (lrSeq: char array) (lrSeqLength: int) (instructions: Map<string,Instruction>) (endKeys: string Set) (key: string) (index: int) =
+    if (endKeys.Contains(key)) then index
     else 
-        let modIndex = if index >= lrSeq.Length then (index % int64(lrSeq.Length)) else index
-        if modIndex > int64(lrSeq.Length) - 1L then printfn "IndexOOF incoming with key %s at step %d" key index 
-        let newKey = 
-            match lrSeq.[(int(modIndex))] with
-            | 'R' -> instructions[key].right
-            | 'L' -> instructions[key].left
-            | _ -> raise (System.ArgumentException("Unknown char"))
-        //printfn "Key %s -> Key %s, Index: %d " key newKey index
-        traverse lrSeq instructions endKey newKey (index + 1L)
+        let (newKey, newIndex) = takeStep lrSeq lrSeqLength instructions key index
+        traverse lrSeq lrSeqLength instructions endKeys newKey newIndex
+
+let multiTraverse (lrSeq: char array) (lrSeqLength: int) (instructions: Map<string,Instruction>) (endKeys: string Set) (keys: string Set) =
+    let rec lcm (a: int64) (b: int64) = if b = 0L then a else (lcm b (a % b))
+    keys 
+    |> Set.map (fun key -> traverse lrSeq lrSeqLength instructions endKeys key 0)
+    |> Set.toList
+    |> List.map int64
+    |> List.reduce (fun a b -> (a * b) / (lcm a b))
+
+let getKeys (instructions: Map<string,Instruction>) =
+    let getKey c = instructions.Keys |> Seq.filter (fun s -> s.Chars(2) = c) |> Set.ofSeq
+    let startKeys = getKey 'A'
+    let endKeys = getKey 'Z'
+    (startKeys, endKeys)
 
 let ((example1lrSeq),(example1instructions: Map<string,Instruction>)) = parseInput "./input/day08_example.txt"
-let ((example2lrSeq),(example2instructions: Map<string,Instruction>))  = parseInput "./input/day08_example2.txt"
+let ((example2lrSeq),(example2instructions: Map<string,Instruction>)) = parseInput "./input/day08_example2.txt"
+let ((example3lrSeq),(example3instructions: Map<string,Instruction>)) = parseInput "./input/day08_example3.txt"
 let (lrSeq, instructions) = parseInput "./input/day08.txt"
 
 let startKey = "AAA"
-let endKey = "ZZZ"
+let endKeySetPart1 = Set.empty.Add("ZZZ")
 
-traverse (example1lrSeq) example1instructions endKey startKey 0 |> printfn "Example answer 1.1: %A"
-traverse (example2lrSeq) example2instructions endKey startKey 0 |> printfn "Example answer 1.2: %A"
-traverse (lrSeq) instructions endKey startKey 0 |> printfn "Answer 1: %A"
+// Part 1
+traverse (example1lrSeq) (example1lrSeq.Length) example1instructions endKeySetPart1 startKey 0 |> printfn "Example answer 1.1: %A"
+traverse (example2lrSeq) (example2lrSeq.Length) example2instructions endKeySetPart1 startKey 0 |> printfn "Example answer 1.2: %A"
+traverse (lrSeq) (lrSeq.Length) instructions endKeySetPart1 startKey 0 |> printfn "Answer 1: %A"
+
+// Part 2
+let (example3StartKeys, example3EndKeys) = getKeys example3instructions
+let (startKeys, endKeys) = getKeys instructions
+multiTraverse example3lrSeq (example3lrSeq.Length) example3instructions example3EndKeys example3StartKeys |> printfn "Example answer 2.1: %A"
+multiTraverse lrSeq (lrSeq.Length) instructions endKeys startKeys |> printfn "Answer 2: %A"
