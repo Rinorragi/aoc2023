4be44b7fc2d6a4d86dafc3a3104e5b51417a3506
Rinorragi <rinorragi@gmail.com>
2023-12-09 08:59:36 +0200

    Day 9 - part 2

--- day09.fsx
+++ day09.fsx
@@ -12,16 +12,32 @@ let parseInput (filePath) =
     |> Array.map (fun s -> s.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)|> Array.map int |> List.ofArray)
     |> List.ofArray 
 
-let rec getPredictionValue  (historyRow: int list) =
+let rec getPredictionValue appender (historyRow: int list) =
     let pairDiffs = 
         historyRow |> List.pairwise |> List.map (fun (first, second) -> second - first)
     let predictionDiff = 
         match (pairDiffs |> List.forall(fun i -> i = 0)) with
         | true -> 0
-        | false -> getPredictionValue pairDiffs
-    historyRow |> List.last |> (+) predictionDiff
+        | false -> getPredictionValue appender pairDiffs
+    historyRow |> appender predictionDiff
+
+let part1Appender diffValue list  = list |> List.last |> (+) diffValue
+let part1Solver values text = 
+    values 
+    |> List.map (fun f -> getPredictionValue part1Appender f) 
+    |> List.sum 
+    |> printfn text
+
+let part2Appender diffValue list  = list |> List.head |> (fun f -> f - diffValue)
+let part2Solver values text = 
+    values 
+    |> List.map (fun f -> getPredictionValue part2Appender f) 
+    |> List.sum 
+    |> printfn text
 
 let instabilitySensorHistoryExamples = parseInput "./input/day09_example.txt"
 let instabilitySensorHistories = parseInput "./input/day09.txt"
-instabilitySensorHistoryExamples |> List.map getPredictionValue |> List.sum |> printfn "Example answer 1: %A"
-instabilitySensorHistories |> List.map getPredictionValue |> List.sum |> printfn "Answer 1: %A"
+part1Solver instabilitySensorHistoryExamples "Example answer 1: %A"
+part1Solver instabilitySensorHistories "Answer 1: %A"
+part2Solver instabilitySensorHistoryExamples "Example answer 2: %A"
+part2Solver instabilitySensorHistories "Answer 2: %A"
