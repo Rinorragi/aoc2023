fb57fce4d652b9eff2cdfc1c62bcc518d058acb2
Joona Immonen <joona.immonen@zure.com>
2023-12-15 08:30:52 +0200

    Day 15: refactor

--- day15.fsx
+++ day15.fsx
@@ -7,16 +7,14 @@ let parseInput (filePath) =
     |> fun s -> s.Split(',', System.StringSplitOptions.RemoveEmptyEntries)
     |> List.ofArray 
 
-let hasher (input: string list) =
-    input
-    |> List.map (fun s -> 
-        s.ToCharArray() 
-        |> List.ofArray
-        |> List.fold (fun state c -> 
-            ((state + (int c)) * 17) % 256 
-        ) 0 )
-    |> List.sum
+let hasher (s: string) =
+    s.ToCharArray() 
+    |> List.ofArray
+    |> List.fold (fun state c -> 
+        ((state + (int c)) * 17) % 256 
+    ) 0
+let p1Solver (input: string list) = input |> List.map hasher |> List.sum
 
-parseInput "./input/day15_example.txt" |> hasher |> printfn "Example answer 1: %d" 
-parseInput "./input/day15.txt" |> hasher |> printfn "Answer 1: %d" 
+parseInput "./input/day15_example.txt" |> p1Solver |> printfn "Example answer 1: %d" 
+parseInput "./input/day15.txt" |> p1Solver |> printfn "Answer 1: %d" 
     
