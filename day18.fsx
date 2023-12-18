57c6e25748e944081c003c0f91caf85df62aff88
Rinorragi <rinorragi@gmail.com>
2023-12-18 10:08:57 +0200

    Day 18: part 2

--- day18.fsx
+++ day18.fsx
@@ -10,24 +10,14 @@ type Direction =
 type Instruction = {
     dir: Direction
     amount: int
-    color: string
 }
-
-type PixelType = 
-    | Empty
-    | Filled
-    | Digged
 
 type Coordinate = {
     x: int
     y: int
-    t: PixelType
-    color: string
 }
 
-let white = "#000000"
-
-let parse pathString =
+let parsePart1 pathString =
     System.IO.File.ReadAllLines pathString
     |> Array.map (fun s -> 
         let sArr = s.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)
@@ -40,89 +30,69 @@ let parse pathString =
                 | "U" -> Direction.North 
                 | _ -> failwith (sprintf "Wrong string: %s" sArr.[0])
             amount = sArr.[1] |> int
-            color = sArr.[2].Substring(1,sArr.[2].Length - 2)
         })
     |> List.ofArray
 
+let parsePart2 pathString =
+    System.IO.File.ReadAllLines pathString
+    |> Array.map (fun s -> 
+        let sArr = s.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)
+        let hexaString = sArr.[2].Substring(2,5)
+        let ins = sArr.[2].Substring(sArr.[2].Length - 2,1)
+        {
+            dir = 
+                match ins with 
+                | "0" -> Direction.East
+                | "1" -> Direction.South 
+                | "2" -> Direction.West  
+                | "3" -> Direction.North 
+                | _ -> failwith (sprintf "Wrong string: %s" ins)
+            amount = System.Convert.ToInt32(hexaString,16)
+        })
+    |> List.ofArray
 
 let instructionsToCoordinates (instructions: Instruction list) =
     let coordinates = 
         instructions
         |> List.fold (fun state ins -> 
             let from = state |> List.last
-            let newCoordinates = 
+            let newCoord = 
                 match ins.dir with 
-                | North ->
-                    [1..ins.amount] |> List.map (fun i -> { x=from.x; y=from.y - i; t=PixelType.Digged; color=ins.color})
-                | South ->
-                    [1..ins.amount] |> List.map (fun i -> { x=from.x; y = from.y + i; t=PixelType.Digged; color = ins.color})
-                | West ->
-                    [1..ins.amount] |> List.map (fun i -> { x=from.x - i; y = from.y; t=PixelType.Digged; color = ins.color})
-                | East -> 
-                    [1..ins.amount] |> List.map (fun i -> { x=from.x + i; y = from.y; t=PixelType.Digged; color = ins.color})
-            state @ newCoordinates
-        ) [{x=0; y=0; t=PixelType.Digged; color=white}]
+                | North -> { x=from.x; y=from.y - ins.amount;}
+                | South -> { x=from.x; y = from.y + ins.amount;}
+                | West -> { x=from.x - ins.amount; y = from.y;}
+                | East -> { x=from.x + ins.amount; y = from.y;}
+            state @ [newCoord]
+        ) [{x=0; y=0;}]
     
     let yMin = coordinates |> List.minBy (fun c -> c.y) |> (fun c -> c.y)
     let xMin = coordinates |> List.minBy (fun c -> c.x) |> (fun c -> c.x)
 
-    coordinates 
-    |> List.map (fun c -> 
-        let newY = if yMin < 0 then c.y + abs(yMin) else c.y
-        let newX = if xMin < 0 then c.x + abs(xMin) else c.x
-        { x=newX; y=newY; t=c.t; color=c.color})
-
-let instructionsToGrid (instructions: Instruction list) =
-    let coordinates = instructionsToCoordinates instructions
-    let coordinateMap = coordinates |> List.map (fun c -> (c.x, c.y),c) |> Map.ofList
-    let yMax = coordinates |> List.maxBy (fun c -> c.y) |> (fun c -> c.y)
-    let xMax = coordinates |> List.maxBy (fun c -> c.x) |> (fun c -> c.x)
-    [0..yMax]
-    |> List.map (fun newY -> 
-        [0..xMax]
-        |> List.fold (fun (pixelArr) newX ->  
-            let pixel = 
-                match coordinateMap.ContainsKey (newX,newY) with
-                | false -> {x=newX;y=newY;t=PixelType.Empty;color=white}
-                | true -> coordinateMap.[(newX,newY)]
-            (pixelArr @ [pixel])
-            ) ([]))
-
-let printGrid (grid: Coordinate list list) =
-    grid
-    |> List.map (fun cList -> 
-        cList 
+    let coordinates = 
+        coordinates 
         |> List.map (fun c -> 
-            match c.t with 
-            | PixelType.Digged -> '#'
-            | PixelType.Filled -> '?'
-            | PixelType.Empty -> '_')
-        |> Array.ofList)
-    |> List.map System.String
-    |> List.map (fun s -> printfn "%s" s)
-
-let solvePart1 (grid: Coordinate list) =
-    let len = (grid.Length |> float) / 2.0
+            let newY = if yMin < 0 then c.y + abs(yMin) else c.y
+            let newX = if xMin < 0 then c.x + abs(xMin) else c.x
+            { x=newX; y=newY;})
+    let insructionsAmount = 
+        System.Math.Round(
+            // + 1 from the starting point
+            (((instructions |> List.sumBy (fun f -> f.amount |> float)) + 1.0) / 2.0),
+            System.MidpointRounding.AwayFromZero)
+    (coordinates, insructionsAmount)
+    
+let calculateArea (coordinates: Coordinate list, len: float) =
     // Shoelace formula for area of polygon. Nigel Galloway: April 11th., 2018
     let shoeLace(n::g) = abs(List.pairwise(n::g@[n])|>List.fold(fun n ((nα,gα),(nβ,gβ))->n+(nα*gβ)-(gα*nβ)) 0.0)/2.0
     //printfn "%f" (shoeLace [(3.0,4.0); (5.0,11.0); (12.0,8.0); (9.0,5.0); (5.0,6.0)])
     let floatAnswer = 
-        grid 
-        |> List.filter (fun c -> c.t = PixelType.Digged)
+        coordinates 
         |> List.map (fun f -> (f.x |> float, f.y |> float))
         |> shoeLace 
         |> (+) len
     System.Math.Round(floatAnswer,System.MidpointRounding.AwayFromZero)
-//    |> List.length
 
-let exampleInput = parse "./input/day18_example.txt"
-//let exampleGrid  = exampleInput |> instructionsToGrid 
-//exampleGrid |> printGrid
-exampleInput |> instructionsToCoordinates |> solvePart1 |> printfn "Example answer 1: %A" 
-
-let input = parse "./input/day18.txt"
-//let grid = input |> instructionsToGrid 
-input |> instructionsToCoordinates |> solvePart1 |> printfn "Example answer 1: %A" 
-//grid |> printGrid
-//grid |> solvePart1 |> printfn "Answer 1: %A" 
-//input |> printfn "%A"
+parsePart1 "./input/day18_example.txt" |> instructionsToCoordinates |> calculateArea |> printfn "Example answer 1: %f" 
+parsePart2 "./input/day18_example.txt" |> instructionsToCoordinates |> calculateArea |> printfn "Example answer 2: %f" 
+parsePart1 "./input/day18.txt" |> instructionsToCoordinates |> calculateArea |> printfn "Answer 1: %f" 
+parsePart2 "./input/day18.txt" |> instructionsToCoordinates |> calculateArea |> printfn "Answer 2: %f" 
